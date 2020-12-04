e747d592e78b3c05c89a1e79a58f5b15bf08ec34
Anton Lykhatskyi <lykhatskyi@gmail.com>
2020-12-04 11:47:50 +0200

    Add custom font

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
+import { useFonts } from 'expo-font';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+const customFonts = {
+  Freshman: require('./assets/fonts/Freshman.ttf'),
+};
+
 export default function App() {
+
+  const [loaded] = useFonts(customFonts);
+
+  if (!loaded) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text style={styles.grey}>Welcome to EasyWorkout Application </Text>
@@ -24,6 +36,6 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 25,
     textAlign: 'center',
-    fontFamily: 'sans-serif-thin',
+    fontFamily: 'Freshman',
   },
 });
