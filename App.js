a7d210644d3436fa1c4befe7454c9753d67e66c8
Anton Lykhatskyi <lykhatskyi@gmail.com>
2020-12-04 14:24:45 +0200

    Add routing and split app by screen

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
 import { useFonts } from 'expo-font';
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Camera from './screens/camera/Camera';
+import Home from './screens/home/Home';
 
 const customFonts = {
   QuicksandBold: require('./assets/fonts/Quicksand-Bold.ttf'),
   QuicksandVariable: require('./assets/fonts/Quicksand-VariableFont_wght.ttf'),
 };
+
+const Stack = createStackNavigator();
 
 export default function App() {
 
@@ -18,26 +22,11 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.grey}>Welcome to EasyWorkout Application </Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>{/* Rest of your app code */}
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Camera" component={Camera} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
-  grey: {
-    color: 'grey',
-    fontWeight: 'bold',
-    fontSize: 25,
-    textAlign: 'center',
-    fontFamily: 'QuicksandBold',
-  },
-});
---
+++ screens/home/styles.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+
+    },
+    grey: {
+        color: 'grey',
+        fontWeight: 'bold',
+        fontSize: 25,
+        textAlign: 'center',
+        fontFamily: 'QuicksandBold',
+    },
+});
