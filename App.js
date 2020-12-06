63c013d845f2fb26b16b337d72f3faa38f688a99
Anton Lykhatskyi <lykhatskyi@gmail.com>
2020-12-06 19:11:16 +0200

    Fix styles error

--- App.js
+++ App.js
@@ -30,20 +30,3 @@ export default function App() {
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
-  grey: {
-    color: 'grey',
-    fontWeight: 'bold',
-    fontSize: 25,
-    textAlign: 'center',
-    fontFamily: 'sans-serif-thin',
-  },
-});
