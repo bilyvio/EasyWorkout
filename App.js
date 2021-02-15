fd5fb316a18f11b29a7874619bcfc321f8f79069
Anton Lykhatskyi <lykhatskyi@gmail.com>
2021-02-15 15:27:39 +0200

    Resolve conflicts

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useFonts, Quicksand_700Bold, Quicksand_500Medium } from '@expo-google-fonts/quicksand';
 import Intro from './src/screens/intro_screen1/intro_screen1';
 import Intro2 from './src/screens/intro_screen2/intro_screen2';
 import Intro3 from './src/screens/intro_screen3/intro_screen3';
@@ -9,8 +11,8 @@ import Details from './src/screens/video_screen/video_screen';
 import SettingsComponent from './src/screens/settings_screen/settings_screen';
 
 import { CameraTitle } from './src/components/CameraTitle/styles';
+import Sup from './src/screens/support_screen/support_screen';
 import { Drawer_Content } from './src/components/Drawer_Content/Drawer_Content';
-import {useFonts, Quicksand_700Bold,Quicksand_500Medium} from "@expo-google-fonts/quicksand";
 
 const headerConfig = {
   title: 'Scan the simulator',
@@ -28,11 +30,7 @@ const headerConfig = {
     color: '#FFFFFF',
     alignSelf: 'center'
   },
-}
-
-import { CameraTitle } from './src/components/CameraTitle/styles';
-import Sup from "./src/screens/support_screen/support_screen";
-
+};
 
 const Drawer = createDrawerNavigator();
 function CameraScreen({ navigation }) {
@@ -56,45 +54,47 @@ export default function App() {
   }
 
   return (
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Sup" component={Sup} options={{headerShown: false}}/>
-          <Stack.Screen name="Intro" component={Intro} options={{headerShown: false}}/>
-          <Stack.Screen name="Intro2" component={Intro2} options={{headerShown: false}}/>
-          <Stack.Screen name="Intro3" component={Intro3} options={{headerShown: false}}/>
-          <Stack.Screen
-              name="Camera"
-              component={CameraContainer}
-              options={{...headerConfig, headerLeft: null}}
-          />
-          <Stack.Screen
-              name="Details"
-              component={Details}
-              options={{...headerConfig, title: 'Identified',
-                headerStyle: {
-                  height: 90,
-                  backgroundColor: '#EE4733',
-                  elevation: 3
-                }
-              }}
-          />
-          <Stack.Screen
-            name="settings"
-            component={SettingsComponent}
-            options={{
-              ...headerConfig,
-              title: 'Settings',
-              headerStyle: {
-                height: 90,
-                backgroundColor: 'var(--theme-color-bg)',
-                elevation: 3
-              },
-              headerTitleStyle: {
-                color: 'var(--theme-color-font)',
-              }
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Intro" component={Intro} options={{ headerShown: false }} />
+        <Stack.Screen name="Intro2" component={Intro2} options={{ headerShown: false }} />
+        <Stack.Screen name="Intro3" component={Intro3} options={{ headerShown: false }} />
+        <Stack.Screen
+          name="Camera"
+          component={CameraScreen}
+          options={{ ...headerConfig, headerLeft: null }}
+        />
+        <Stack.Screen
+          name="Details"
+          component={Details}
+          options={{
+            ...headerConfig,
+            title: 'Identified',
+            headerStyle: {
+              height: 90,
+              backgroundColor: '#EE4733',
+              elevation: 3
+            }
+          }}
+        />
+        <Stack.Screen name="Sup" component={Sup} options={{ headerShown: false }} />
+        <Stack.Screen
+          name="settings"
+          component={SettingsComponent}
+          options={{
+            ...headerConfig,
+            title: 'Settings',
+            headerStyle: {
+              height: 90,
+              backgroundColor: 'var(--theme-color-bg)',
+              elevation: 3
+            },
+            headerTitleStyle: {
+              color: 'var(--theme-color-font)',
+            }
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
